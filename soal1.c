b1ebaf791d1085ff4a9e708a11c8962619e0f1b6
dllus <13224029@mahasiswa.itb.ac.id>
2026-04-27 09:12:17 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,69 @@
-#include <stdio.h>
+/** Praktikum Pemecahan Masalah Dengan Pemrograman 2025/2026
+ * Modul: 3 - Strcuctures dan Dynamic Arrays
+ * Nama: Adlu Naafi Firdaus
+ * nim: 13224029
+ * Nama_File: Soal1_Modul3.c
+ * Soal: 1
+ * Deskripsi: Menentukan Frekuensi data, Mengurutkan data dan median
+ */
 
-int main(void) {
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int *data = NULL;
+    int input, n = 0;
+    int capacity = 0;
+
+    printf("Masukkan data:\n");
+
+    while (1) {
+        scanf("%d", &input);
+        if (input == -1) break;
+
+        if (n >= capacity) {
+            capacity = (capacity == 0) ? 1 : capacity * 2;
+            int *temp = realloc(data, capacity * sizeof(int));
+            if (temp == NULL) {
+                printf("Memory allocation failed!\n");
+                free(data);
+                return 1;
+            }
+            data = temp;
+        }
+        data[n] = input;
+        n++;
+    }
+
+    if (n == 0) {
+        printf("Tidak ada data yang dimasukkan.\n");
+        return 0;
+    }
+
+    qsort(data, n, sizeof(int), compare);
+
+    printf("\nUrutan yang Benar: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", data[i]);
+    }
+
+    printf("\nTotal Data: %d\n", n);
+
+    double median;
+    if (n % 2 == 0) {
+        median = (double)(data[n / 2 - 1] + data[n / 2]) / 2.0;
+    } else {
+        median = (double)data[n / 2];
+    }
+    printf("Median: %.2f\n", median);
+
+    free(data);
+
     return 0;
 }
